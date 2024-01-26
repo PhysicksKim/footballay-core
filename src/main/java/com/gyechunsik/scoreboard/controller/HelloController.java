ca06ccf8744529cb7f0e700f5628b1d801d1acd2
physicksKim <physicksKim@gmail.com>
2024-01-26 19:08:25 +0900

    init commit

---
+++ src/main/java/com/gyechunsik/scoreboard/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.gyechunsik.scoreboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hellopage.html";
+    }
+
+    @GetMapping("/scoreboard")
+    public String scoreboard() {
+        return "scoreboard/index.html";
+    }
+}
