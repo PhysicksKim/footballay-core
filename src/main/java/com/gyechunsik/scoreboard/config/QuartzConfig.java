94bafd59dcc626fa9fc3eb2d1874d0d9e51137c1
PhysicksKim <physickskim@gmail.com>
2025-04-21 12:19:11 +0900

    chore: set more hikari maximum pool size

--- src/main/java/com/gyechunsik/scoreboard/config/QuartzConfig.java
+++ src/main/java/com/gyechunsik/scoreboard/config/QuartzConfig.java
@@ -17,6 +17,8 @@ public class QuartzConfig {
     @Autowired
     private SpringBeanJobFactory springBeanJobFactory;
 
+    // TODO : Bean 으로 직접 넣으면 안됩니다. 이렇게 하면 yml 에서 설정한 값 대신 모두 기본값인 factory 가 덮어씌워집니다.
+    // 예를 들어 thread count 를 5로 설정했으나 기본값인 10으로 들어가게 됩니다.
     @Bean
     public SchedulerFactoryBean schedulerFactoryBean() {
         SchedulerFactoryBean factory = new SchedulerFactoryBean();
