997ed019b268071cbce23b675bc2c54bdcedee5b
PhysicksKim <physickskim@gmail.com>
2025-05-01 16:40:48 +0900

    feat: split index controllers for gyechunhoe.com and footballay.com

--- src/main/java/com/gyechunsik/scoreboard/web/gyechunhoe/controller/GyeIndexController.java
+++ src/main/java/com/gyechunsik/scoreboard/web/gyechunhoe/controller/GyeIndexController.java
@@ -1,4 +1,4 @@
-package com.gyechunsik.scoreboard.web.common.controller;
+package com.gyechunsik.scoreboard.web.gyechunhoe.controller;
 
 import com.gyechunsik.scoreboard.config.AppEnvironmentVariable;
 import lombok.RequiredArgsConstructor;
@@ -7,24 +7,26 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Controller
+@RequestMapping("/gyechunhoe")
 @RequiredArgsConstructor
-public class IndexPageController {
+public class GyeIndexController {
 
     private final RestTemplate restTemplate;
 
     private final AppEnvironmentVariable envVar;
 
-    @GetMapping("/")
-    public ResponseEntity<String> footballayIndexPage() {
+    @GetMapping
+    public ResponseEntity<String> gyechunhoeIndexPage() {
         String path = "https://static."+envVar.getDomain()+"/indexpage/index.html";
         String html = restTemplate.getForObject(path, String.class);
-        log.info("footballay main Page");
+        log.info("gyechunhoe main Page");
         return ResponseEntity.ok()
                 .contentType(new MediaType(MediaType.TEXT_HTML, StandardCharsets.UTF_8))
                 .body(html);
